21df553a1be58b36c62942adffda28155117c3db
Iftekhar-007 <shawonsp534@gmail.com>
2025-11-26 18:16:05 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+// import path from "path";
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("server is runnign.....");
+
+    if (req.url == "/" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "hello0 from typescript....",
+          path: req.url,
+        })
+      );
+    }
+  }
+);
+
+server.listen(5000, () => {
+  console.log(`server is running on ${5000}`);
+});
